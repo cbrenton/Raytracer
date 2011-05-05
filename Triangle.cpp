fbe72151c437a3b05014a4bde139d8e836099383
Chris <cbrenton@calpoly.edu>
2011-05-05 15:04:28 -0700

    Triangle in progress, switching to lab computers. Broken for now.

--- Triangle.cpp
+++ Triangle.cpp
@@ -7,6 +7,7 @@
 
 #include <string>
 #include "Triangle.h"
+#include "Matrix.h"
 
 Triangle::Triangle(std::istream& input)
 {
@@ -28,6 +29,33 @@ Triangle::Triangle(std::istream& input)
 
 bool Triangle::hit(Ray ray, float *t, float minT, float maxT)
 {
+   Matrix4 A = new Matrix4(
+         location.x() - corner2.x(), location.x() - corner3.x(), ray.dir.x(),
+         location.y() - corner2.y(), location.y() - corner3.y(), ray.dir.y(),
+         location.z() - corner2.z(), location.z() - corner3.z(), ray.dir.z());
+   Matrix4 baryT = new Matrix4(
+         location.x() - corner2.x(), location.x() - corner3.x(), location.x() - ray.point.x(),
+         location.y() - corner2.y(), location.y() - corner3.y(), location.y() - ray.point.y(),
+         location.z() - corner2.z(), location.z() - corner3.z(), location.z() - ray.point.z();
+   Matrix4 baryBeta = new Matrix4(
+         location.x() - ray.point.x(), location.x() - corner3.x(), ray.dir.x(),
+         location.y() - ray.point.y(), location.y() - corner3.y(), ray.dir.y(),
+         location.z() - ray.point.z(), location.z() - corner3.z(), ray.dir.z());
+   Matrix4 baryGamma = new Matrix4(
+         location.x() - corner2.x(), location.x() - ray.point.x(), ray.dir.x(),
+         location.y() - corner2.y(), location.y() - ray.point.y(), ray.dir.y(),
+         location.z() - corner2.z(), location.z() - ray.point.z(), ray.dir.z());
+   float bAlpha = baryAlpha.det() / A.det();
+   float bGamma = baryGamma.det() / A.det();
+   float bT = baryT.det() / A.det();
+   if (bT < 0 || bT > F_MAX)
+   {
+   ;
+   }
+   delete A;
+   delete baryT;
+   delete baryBeta;
+   delete baryGamma;
    return false;
 }
 
